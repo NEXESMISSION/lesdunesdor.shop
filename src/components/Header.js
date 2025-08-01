af82d31d23b0518260ebcbb2a7f2dc6cc3b0cea3
NEXESMISSION <your-email@example.com>
2025-08-01 13:11:24 +0100

    Update logo to redirect to store page instead of landing page

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
       <div className="max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-20">
           <div className="flex-shrink-0">
-            <Link to="/" className="flex items-center group">
+            <Link to="/store" className="flex items-center group">
               <img 
                 src="/meubledor-logo.png" 
                 alt="Meubles D'Or Logo" 
